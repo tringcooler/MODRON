a9372667facaf2468b5fbe48c8f0a658c57bd4b1
tringcooler <tringcooler@users.noreply.github.com>
2022-03-25 15:16:20 +0800

    Update errtrans.py
    用这种方式临时修改sys.path，以免对于其他模块产生影响。今后使用sys.path进行模块导入时的相对地址定位时，也应该使用这种方式。

--- cmplr/trans/errtrans.py
+++ cmplr/trans/errtrans.py
@@ -5,8 +5,11 @@ try:
     from errparse import err_parse
 except:
     import sys
-    sys.path.append('..')
+    #sys.path.append('..')
+    opath = sys.path
+    sys.path = ['..']
     from errparse import err_parse
+    sys.path = opath
 
 class err_trans(err_parse):
     pass
